fe9115d57c7a504d398affe78e6f289813af489b
ara8586 <ara@mailfa.com>
2017-02-06 23:54:24 +0330

    Update setlangfa.lua

--- plugins/setlangfa.lua
+++ plugins/setlangfa.lua
@@ -2,34 +2,35 @@ do
 local function run(msg, matches, callback, extra)
 local hash = 'group:'..msg.to.id
 local group_lang = redis:hget(hash,'lang')
-if matches[1] == 'نشاندن زبان' and  matches[2] == 'انگلیسی' and is_owner(msg) then 
+if matches[1] == 'تنظیم زبان' and  matches[2] == 'انگلیسی' and is_owner(msg) then 
     
    redis:hdel(hash,'lang')
-        return 'group lang set to : en'
+        return 'group lang set to Engish'
 end
 
 
 
-if matches[1] == 'نشاندن زبان' and matches[2] == 'فارسی' and is_owner(msg) then
+if matches[1] == 'تنظیم زبان' and matches[2] == 'فارسی' and is_owner(msg) then
 redis:hset(hash,'lang',matches[2])
-        return 'زبان گروه تنظیم شد به : FA'
+        return 'زبان گروه تنظیم شد به فارسی'
 end
 if matches[1] == 'زبان' then
 if group_lang then 
 return "زبان گروه شما هم اکنون بر روی فارسی قرار دارد"
 else
-return "Group lang : en"
+return "Group lang English"
 end
 end
 end
 return {
   patterns = {
-    "^[!#/](نشاندن زبان) (فارسی)$",
-  "^[!#/](نشاندن زبان) (انگلیسی)$",
+    "^[!#/](تنظیم زبان) (فارسی)$",
+  "^[!#/](تنظیم زبان) (انگلیسی)$",
   "^[!#/](زبان)"
   },
   run = run
 }
 end
---edit by @CliApi
---Our channel : @create_antispam_bot
+
+-- by @mr_ahmadix
+-- sp @suport_arabot

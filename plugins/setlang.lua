35b77391c01a8296056c51d18c4fb1d68b0631a9
ara8586 <ara@mailfa.com>
2017-02-06 23:49:09 +0330

    Update setlang.lua

--- plugins/setlang.lua
+++ plugins/setlang.lua
@@ -5,20 +5,20 @@ local group_lang = redis:hget(hash,'lang')
 if matches[1] == 'setlang' and  matches[2] == 'en' and is_owner(msg) then 
     
    redis:hdel(hash,'lang')
-        return 'group lang set to : en'
+        return 'group lang set to Engish'
 end
 
 
 
 if matches[1] == 'setlang' and matches[2] == 'fa' and is_owner(msg) then
 redis:hset(hash,'lang',matches[2])
-        return 'زبان گروه تنظیم شد به : FA'
+        return 'زبان گروه تنظیم شد به فارسی'
 end
 if matches[1] == 'lang' then
 if group_lang then 
 return "زبان گروه شما هم اکنون بر روی فارسی قرار دارد"
 else
-return "Group lang : en"
+return "Group lang English"
 end
 end
 end
